54bc7c594ce8caad3b43156c45bf4c1ae8475a98
luciano de franca <lucianodefranca86@gmail.com>
2022-03-16 23:11:05 -0300

    Configuracao smtp gmail, endpoint sending-email

--- src/main/java/com/ms/email/services/EmailService.java
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -1,5 +1,6 @@
 package com.ms.email.services;
 
+import com.ms.email.models.EmailModel;
 import com.ms.email.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,7 @@ public class EmailService {
 
     @Autowired
     private EmailRepository repository;
+
+    public void sendEmail(EmailModel emailModel) {
+    }
 }
